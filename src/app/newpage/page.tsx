82c8e40503edf40c4066bc3fcebf57c8f9d285bd
thangpd <thangpd@smartosc.com>
2024-04-01 10:52:27 +0700

    Alert 1

--- src/app/newpage/page.tsx
+++ src/app/newpage/page.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 type Props = {}
 
 const NewPage = (props: Props) => {
+  const handAlert = () => {
+    console.log("HELLO WORLD");
+  }
+  const handAlert1 = () => {
+    console.log("HELLO WORLD");
+  }
+  const handAlert2 = () => {
+    console.log("HELLO WORLD");
+  }
   return (
     <div>NewPage</div>
   )
